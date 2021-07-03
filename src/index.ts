d9887ddc0574503bfeb980e4233b99bff95836c9
Duc Nguyen <minhduc140583@gmail.com>
2021-07-03 17:14:03 +0700

    Remove catch

--- src/index.ts
+++ src/index.ts
@@ -20,42 +20,22 @@ export class HttpRequest {
   }
 
   get<T>(url: string, options?: {headers?: Headers}): Promise<T> {
-    return Promise.resolve(
-      this.axios.get<T>(url, options ? options : this.getOptions())
-      .then(({data}) => data)
-      .catch(({data}) => { throw data; })
-    );
+    return Promise.resolve(this.axios.get<T>(url, options ? options : this.getOptions()).then(({data}) => data));
   }
 
   delete<T>(url: string, options?: {headers?: Headers}): Promise<T> {
-    return Promise.resolve(
-      this.axios.delete(url, options ? options : this.getOptions())
-      .then(({data}) => data)
-      .catch(({data}) => { throw data; })
-    );
+    return Promise.resolve(this.axios.delete(url, options ? options : this.getOptions()).then(({data}) => data));
   }
 
   post<T>(url: string, obj: any, options?: {headers?: Headers}): Promise<T> {
-    return Promise.resolve(
-      this.axios.post(url, obj, options ? options : this.getOptions())
-      .then(({data}) => data)
-      .catch(({data}) => { throw data; })
-    );
+    return Promise.resolve(this.axios.post(url, obj, options ? options : this.getOptions()).then(({data}) => data));
   }
 
   put<T>(url: string, obj: any, options?: {headers?: Headers}): Promise<T> {
-    return Promise.resolve(
-      this.axios.put(url, obj, options ? options : this.getOptions())
-      .then(({data}) => data)
-      .catch(({data}) => { throw data; })
-    );
+    return Promise.resolve(this.axios.put(url, obj, options ? options : this.getOptions()).then(({data}) => data));
   }
 
   patch<T>(url: string, obj: any, options?: {headers?: Headers}): Promise<T> {
-    return Promise.resolve(
-      this.axios.patch<T>(url, obj, options ? options : this.getOptions())
-      .then(({data}) => data)
-      .catch(({data}) => { throw data; })
-    );
+    return Promise.resolve(this.axios.patch<T>(url, obj, options ? options : this.getOptions()).then(({data}) => data));
   }
 }
